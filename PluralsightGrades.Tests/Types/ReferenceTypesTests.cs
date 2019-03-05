098c43673f9db9e2d84c475cc817275e34800922
Jackie Pearce <jackiepearce83@hotmail.com>
2019-03-05 15:24:49 +0000

    Tests that show the behaviour of reference and value types, and the use of enum in a stringComparisonTest

--- PluralsightGrades.Tests/Types/ReferenceTypesTests.cs
+++ PluralsightGrades.Tests/Types/ReferenceTypesTests.cs
@@ -11,7 +11,29 @@ namespace PluralsightGrades.Tests.Types
     public class ReferenceTypesTests
     {
         [TestMethod]
-        public void VariablesHoldAReference()
+        public void StringComparisonTest()
+        {
+            string name1 = "Rob";
+            string name2 = "rob";
+
+            bool result = String.Equals(name1, name2, StringComparison.InvariantCultureIgnoreCase);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IntVariablesHoldAValue()
+        {
+            int x1 = 100;
+            int x2 = x1;
+
+            x1 = 4;
+            Assert.AreNotEqual(x1, x2);
+            Assert.AreEqual(4, x1);
+            Assert.AreEqual(100, x2);
+        }
+
+        [TestMethod]
+        public void GradeBookVariablesHoldAReference()
         {
             GradeBook g1 = new GradeBook();
             GradeBook g2 = g1;
